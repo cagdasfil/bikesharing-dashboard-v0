ccfbcb769f17b5f7db0783733486074c2dcc309e
CAGDAS FIL <e2093839@ceng.metu.edu.tr>
2020-03-01 02:00:01 +0300

    Login page implemented.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 import './App.css';
 
 function App() {
+
+  const [admin, setAdmin] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return admin.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="LoginBoxIn">
+          <form onSubmit={handleSubmit} className="Form">
+            <Form.Label className="FormLabels">Admin</Form.Label>
+            <Form.Control
+              autoFocus
+              type="admin"
+              value={admin}
+              onChange={e => setAdmin(e.target.value)}
+              className="FormBoxes"
+            />
+            <Form.Label className="FormLabels">Password</Form.Label>
+            <Form.Control
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              type="password"
+              className="FormBoxes"
+            />
+            <Button className="Button" block bsSize="large" disabled={!validateForm()} type="submit">
+              Login
+            </Button>
+          </form>
+        </div>
     </div>
   );
 }
