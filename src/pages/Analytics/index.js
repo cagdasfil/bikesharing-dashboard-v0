3aa2cb13f9612618423e1b776114bb7a26b1cd31
CAGDAS FIL <e2093839@ceng.metu.edu.tr>
2020-03-02 20:26:13 +0300

    analytics and database pages implemented as template.

--- src/pages/Analytics/index.js
+++ src/pages/Analytics/index.js
@@ -1,14 +1,94 @@
-import React, { useState } from "react";
-import { Button, Form } from "react-bootstrap";
-import history from "../../services/history";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import PieChartIcon from '@material-ui/icons/PieChart';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import TimelineIcon from '@material-ui/icons/Timeline';
+
+const drawerWidth = 300;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+}));
 
 function Analytics() {
+    const classes = useStyles();
 
-  return (
-    <div>
-
-    </div>
-  );
+    return (
+        <div className={classes.root}>
+          <CssBaseline />
+          <AppBar position="fixed" className={classes.appBar}>
+            <Toolbar>
+              <Typography variant="h6" noWrap>
+                Data Analytics
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            anchor="left"
+          >
+            <div className={classes.toolbar} />
+            <Divider />
+            <List>
+              {['Pie Charts', 'Bar Graphs', 'Timelines'].map((text, index) => (
+                <ListItem button key={text}>
+                  <ListItemIcon> 
+                   {
+                    index === 0 ? <PieChartIcon/> : null
+                   }
+                   {
+                    index === 1 ? <BarChartIcon/> : null
+                   }
+                   {
+                    index === 2 ? <TimelineIcon/> : null
+                   }
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItem>
+              ))}
+            </List>
+            <Divider />
+          </Drawer>
+          <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <div style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
+                Visualized Data
+            </div>
+          </main>
+        </div>
+      );
 }
-
 export default Analytics;
