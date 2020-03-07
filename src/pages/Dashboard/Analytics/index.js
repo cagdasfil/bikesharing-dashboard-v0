43542a13672c103ae1724bf61e99e6224cf0f437
CAGDAS FIL <e2093839@ceng.metu.edu.tr>
2020-03-07 23:09:12 +0300

    Database and Analytics separated as folders. Drawer menu components converted to expandable components.

---
+++ src/pages/Dashboard/Analytics/index.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import MuiExpansionPanel from '@material-ui/core/ExpansionPanel';
+import MuiExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import MuiExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import List from '@material-ui/core/List';
+
+import PieChartIcon from '@material-ui/icons/PieChart';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import TimelineIcon from '@material-ui/icons/Timeline';
+
+const ExpansionPanel = withStyles({
+  root: {
+    border: '1px solid rgba(0, 0, 0, .125)',
+    boxShadow: 'none',
+    '&:not(:last-child)': {
+      borderBottom: 0,
+    },
+    '&:before': {
+      display: 'none',
+    },
+    '&$expanded': {
+      margin: 'auto',
+    },
+  },
+  expanded: {},
+})(MuiExpansionPanel);
+
+const ExpansionPanelSummary = withStyles({
+  root: {
+    backgroundColor: 'rgba(0, 0, 0, .03)',
+    borderBottom: '1px solid rgba(0, 0, 0, .125)',
+    marginBottom: -1,
+    minHeight: 56,
+    '&$expanded': {
+      minHeight: 56,
+    },
+  },
+  content: {
+    '&$expanded': {
+      margin: '12px 0',
+    },
+  },
+  expanded: {},
+})(MuiExpansionPanelSummary);
+
+const ExpansionPanelDetails = withStyles(theme => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiExpansionPanelDetails);
+
+export default function Analytics() {
+  const [expanded, setExpanded] = React.useState('panel1');
+
+  const handleChange = panel => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+
+  return (
+    <div>
+      <ExpansionPanel square expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <ListItemIcon>
+            <BarChartIcon />
+          </ListItemIcon>
+          <ListItemText primary="Analytics"/>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <List>
+            {['Pie Charts', 'Bar Graphs', 'Timelines'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon> 
+                  {
+                    index === 0 ? <PieChartIcon/> : null
+                  }
+                  {
+                    index === 1 ? <BarChartIcon/> : null
+                  }
+                  {
+                    index === 2 ? <TimelineIcon/> : null
+                  }
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </div>
+  );
+}
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -3,57 +3,20 @@ import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
+
+import Database from './Database';
+import Analytics from './Analytics';
+import Bikes from './components/Bikes';
+
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import Collapse from '@material-ui/core/Collapse';
-
-
-
-import StorageIcon from '@material-ui/icons/Storage';
-import PersonIcon from '@material-ui/icons/Person';
-import DirectionsBikeIcon from '@material-ui/icons/DirectionsBike';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
-import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
-import Bikes from './components/Bikes';
-import PieChartIcon from '@material-ui/icons/PieChart';
-import BarChartIcon from '@material-ui/icons/BarChart';
-import TimelineIcon from '@material-ui/icons/Timeline';
-
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
-  },
-  appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
   },
   hide: {
     display: 'none',
@@ -88,9 +51,6 @@ const useStyles = makeStyles(theme => ({
     }),
     marginLeft: 0,
   },
-  nested: {
-    paddingLeft: theme.spacing(5),
-  },
 }));
 
 export default function Dashboard() {
@@ -110,16 +70,16 @@ export default function Dashboard() {
     <div className={classes.root}>
       <CssBaseline />
       <div className={classes.drawerHeader}>
-      <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            //edge="start"
-            className={open && classes.hide}
-          >
-            <ChevronRightIcon />
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          //edge="start"
+          className={open && classes.hide}
+        >
+          <ChevronRightIcon />
         </IconButton>
-        </div>
+      </div>
       <Drawer
         className={classes.drawer}
         variant="persistent"
@@ -129,70 +89,17 @@ export default function Dashboard() {
           paper: classes.drawerPaper,
         }}
       >
-          <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
+        <div className={classes.drawerHeader}>
+        <IconButton onClick={handleDrawerClose}>
+          {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+        </IconButton>
         </div>
-            <List>
-                <ListItem button>
-                    <ListItemIcon>
-                        <StorageIcon/>
-                    </ListItemIcon>
-                    <ListItemText primary="Collections"/>
-                </ListItem>
-                <Collapse in={open} timeout="auto" unmountOnExit>
-                    <List component="div" disablePadding>
-                    <ListItem button className={classes.nested}>
-                        <ListItemIcon>
-                            <PersonIcon/>
-                        </ListItemIcon>
-                        <ListItemText primary="Users" />
-                    </ListItem>
-                    <ListItem button className={classes.nested}>
-                        <ListItemIcon>
-                            <DirectionsBikeIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Bikes" />
-                    </ListItem>
-                    <ListItem button className={classes.nested}>
-                        <ListItemIcon>
-                            <LocationOnIcon/>
-                        </ListItemIcon>
-                        <ListItemText primary="Dockers" />
-                    </ListItem>
-                    <ListItem button className={classes.nested}>
-                        <ListItemIcon>
-                            <MonetizationOnIcon/>
-                        </ListItemIcon>
-                        <ListItemText primary="Transactions" />
-                    </ListItem>
-                    </List>
-                </Collapse>
-            </List>
-            <Divider />
-            <List>
-              {['Pie Charts', 'Bar Graphs', 'Timelines'].map((text, index) => (
-                <ListItem button key={text}>
-                  <ListItemIcon> 
-                   {
-                    index === 0 ? <PieChartIcon/> : null
-                   }
-                   {
-                    index === 1 ? <BarChartIcon/> : null
-                   }
-                   {
-                    index === 2 ? <TimelineIcon/> : null
-                   }
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItem>
-              ))}
-            </List>
+        <Database/>
+        <Analytics />
       </Drawer>
       <main
         className={clsx(classes.content, {
-          [classes.contentShift]: open,
+        [classes.contentShift]: open,
         })}
       >
         <Bikes />
