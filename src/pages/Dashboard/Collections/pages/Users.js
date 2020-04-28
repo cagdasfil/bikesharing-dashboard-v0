25844e4137c8bad583a067ded981e13045afdb1a
Dogan Sezgin <e2099299@ceng.metu.edu.tr>
2020-04-28 21:57:03 +0300

    Users Page Has Been Integrated to Database

--- src/pages/Dashboard/Collections/pages/Users.js
+++ src/pages/Dashboard/Collections/pages/Users.js
@@ -47,31 +47,24 @@ const BootstrapInput = withStyles((theme) => ({
   },
 }))(InputBase);
 
-const useStyles = makeStyles((theme) => ({
-  margin: {
-    margin: theme.spacing(1),
-  },
-}));
-
-
 
 
 
 export default class Users extends React.Component{
 
 
-
     constructor(props){
         super(props);
         this.state={
           user:[],
-          role:""
+          role:10,
+          changing_id:""
         };
       }
 
     
     handleChange = (event) => {
-      this.setState({role:event.target.value});
+      this.setState({role:event.target.value});      
     };
   
       columns = [
@@ -116,7 +109,6 @@ export default class Users extends React.Component{
               });
           }
           this.setState({user:formattedData});
-          console.log('Hi Everybody', data);
       };
     
       getData(){
@@ -125,16 +117,56 @@ export default class Users extends React.Component{
           headers : {'Content-Type':'application/json',
           'Authorization': `Bearer ${this.props.jwt}`},
         })
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                this.handleUserData(data);
-            });
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          this.handleUserData(data);
+        });
       }
   
-      componentWillMount(){
+      componentDidMount(){
         this.getData();
+      }
+
+      sendAdminPost(){
+        fetch('http://35.189.94.121/application/setAdmin/', {
+          method : 'post',
+          headers : {'Content-Type':'application/json',
+          'Authorization': `Bearer ${this.props.jwt}`},
+          body : JSON.stringify({userId: this.state.changing_id
+          }),
+        })
+        .then((response) => {
+          this.getData();
+          return response.json();
+        })
+        .catch(error => {
+          console.error('There was an error!', error);
+        });
+      }
+
+      sendAuthenticatedPost(){
+        fetch('http://35.189.94.121/application/setAuthenticated/', {
+          method : 'post',
+          headers : {'Content-Type':'application/json', 'Authorization': `Bearer ${this.props.jwt}`},
+          body : JSON.stringify({ userId: this.state.changing_id
+          }),
+        }).then((response) => {
+          this.getData();
+          return response.json();
+        })
+        .catch(error => {
+          console.error('There was an error!', error);
+        });
+      }
+
+      send(rol){
+        if (this.state.role === 10) {
+          this.sendAdminPost();
+        } else {
+          this.sendAuthenticatedPost();        
+        }
       }
       
     render(){
@@ -143,30 +175,30 @@ export default class Users extends React.Component{
             <CollectionTable columns={this.columns} rows={this.state.user} />
 
             <div >
-            <FormControl >
-              <InputLabel htmlFor="demo-customized-textbox">User Id</InputLabel>
-              <BootstrapInput id="demo-customized-textbox" />
-            </FormControl>
-            <FormControl >
-              <InputLabel id="demo-customized-select-label">Select Role</InputLabel>
-              <Select
-                labelId="demo-customized-select-label"
-                id="demo-customized-select"
-                value={this.state.role}
-                onChange={this.handleChange}
-                input={<BootstrapInput />}
-                displayEmpty
-              >
-                <MenuItem value="">Admin</MenuItem>
-                <MenuItem value={10}>Authenticated</MenuItem>
-              </Select>
-              
+              <FormControl >
+                <InputLabel htmlFor="demo-customized-textbox">User Id</InputLabel>
+                <BootstrapInput id="demo-customized-textbox" 
+                onChange={e => this.setState({changing_id : e.target.value})}
+                />
               </FormControl>
-              <Button className="saveButton"  block bsSize="large" type="submit" >
-                          Save
-                      </Button>
-              
-          </div>
+              <FormControl >
+                <InputLabel id="demo-customized-select-label">Select Role</InputLabel>
+                <Select
+                  labelId="demo-customized-select-label"
+                  id="demo-customized-select"
+                  value={this.state.role}
+                  onChange={this.handleChange}
+                  input={<BootstrapInput />}
+                  displayEmpty
+                >
+                  <MenuItem value={10}>Admin</MenuItem>
+                  <MenuItem value={20}>Authenticated</MenuItem>
+                </Select>
+              </FormControl>
+              <Button className="saveButton"  block bsSize="large" type="submit" onClick={() => this.send(this.state.role)}>
+                Save
+              </Button>
+            </div>
           </div>
           );
     }
